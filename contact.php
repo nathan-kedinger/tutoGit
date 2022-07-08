314fa13227f67fe7484324061b833b297c493948
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2022-07-08 15:38:07 +0200

    add new files

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php require '../front_const/header_const.php';
+    $pageTitle = CONTACT;
+    require_once '../content/head.php';
+    require_once '../content/header.php';
+?>
+
+<body>
+    <main>    
+       
+        <?php require_once '../content/form.php';
+        ?>
+    </main>
+</body>
+
+
+
+
+
+
+
+<?php require_once '../content/footer.php';?>
